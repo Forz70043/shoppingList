5f9a9bda0e23f22f7ab3dfab082734da7ca9fa76
Forz70043 <alfonso.pisicchio@libero.it>
2025-03-08 22:29:19 +0100

    added middleware for users verification

---
+++ middleware/auth.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+
+const verifyToken = (req, res, next) => {
+    const token = req.header('Authorization')?.replace('Bearer ', '');
+    if (!token) {
+        return res.status(401).json({ message: 'Access denied, token missing' });
+    }
+
+    try {
+        const payload = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = payload; // Aggiungi l'utente al request
+        next();
+    } catch (error) {
+        res.status(401).json({ message: 'Token not valid' });
+    }
+};
+
+module.exports = verifyToken;
