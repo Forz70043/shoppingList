a31d08869654cca43892ac75e1c6efdffcc5a722
Forz70043 <alfonso.pisicchio@libero.it>
2025-03-08 22:29:34 +0100

    added models users

---
+++ models/User.js
@@ -0,0 +1,40 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+const bcrypt = require('bcryptjs');
+
+const User = sequelize.define('User', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    }
+}, {
+  tableName: 'users',
+  timestamps: true,
+});
+
+// Hash della password prima di salvare
+User.beforeCreate(async (user) => {
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+});
+
+module.exports = User;
