620c7b07f4c1a02654cd15916fe1369368a8af47
Forz70043 <alfonso.pisicchio@libero.it>
2025-03-08 21:40:31 +0100

    Added main server file

---
+++ server.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const sequelize = require('./config/db');
+const listRoutes = require('./routes/lists');
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/lists', listRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('API Grocery List');
+});
+
+sequelize.sync({ force: false })
+  .then(() => console.log('Database sync'))
+  .catch(err => console.error('Error on sync:', err));
+
+
+app.listen(PORT, () => console.log(`Server start on http://localhost:${PORT}`));
